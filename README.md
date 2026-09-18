c8c4d9cb101c3ca141e8c4124283852ebcb49935
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-09-18 23:52:23 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Zara Launch Client
---
+++ src/app/layout.js
@@ -0,0 +1,34 @@
+import { Roboto_Slab, Patua_One } from "next/font/google";
+import "./globals.css";
+
+const robotoSlab = Roboto_Slab({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    variable: "--font-roboto-slab",
+    display: "swap",
+});
+
+const patuaOne = Patua_One({
+    subsets: ["latin"],
+    weight: ["400"],
+    variable: "--font-patua",
+    display: "swap",
+});
+
+export const metadata = {
+    title: "Zara Launch",
+    description: "",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html
+            lang="en"
+            className={`${robotoSlab.variable} ${patuaOne.variable} h-full antialiased`}
+        >
+            <body className="min-h-full dark:bg-black">
+                {children}
+            </body>
+        </html>
+    );
+}
